0a67dde3840ff0ee43f2ca4d60e92746ed36c51a
0xflotus <0xflotus@gmail.com>
2018-12-12 04:21:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+require("request")(
+  `https://github.com/${process.argv[2] || "0xflotus"}`,
+  (_, __, body) =>
+    console.log(
+      require("cheerio")
+        .load(body)("h2.f4.text-normal.mb-2")
+        .get(1)
+        .children[0].data.trim()
+    )
+);
